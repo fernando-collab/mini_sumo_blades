f992029afef7c55d4e7605184935efda139f9831
fernando-collab <zaspido@hotmail.com>
2023-01-01 22:36:16 +0100

    Se quito .vscode y se hizo un .h

---
+++ src/control_robot.h
@@ -0,0 +1,9 @@
+//definicion de los pines
+#define s_izq 9   //tcrt 5000 sensor de linea
+#define s_drch 10 //tcrt 5000 sensor de linea
+#define s_at_izq 7//fc-51 sensor de ataque
+#define s_at_cnt 8   //fc-51 sensor de ataque
+#define s_at_drch 2  //fc-51 sensor de ataque
+#define boton 4
+#define motor_izq 6//servomotor mg90s
+#define motor_drch 5  //servomotor mg90s
